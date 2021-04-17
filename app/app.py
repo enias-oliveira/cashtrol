51eb45920c656f55065d51ee33f4478caec22cf8
Enias Oliveira <eniasoliveira27@gmail.com>
2021-04-16 21:12:31 -0300

    chore: adds database setup related config

--- app/app.py
+++ app/app.py
@@ -4,6 +4,13 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    from app import configurations
+    from app.configurations import database, migration
+
+    configurations.init_app(app)
+    database.init_app(app)
+    migration.init_app(app)
+
     @app.route("/")
     def hello_flask():
         return {"msg": "Hello Flask!"}, 200

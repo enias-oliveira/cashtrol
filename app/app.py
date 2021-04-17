676e02ef332599051676e6bd6ee961c9c8641a4c
Enias Oliveira <eniasoliveira27@gmail.com>
2021-04-16 21:25:34 -0300

    chore: adds routing configuration

--- app/app.py
+++ app/app.py
@@ -4,12 +4,13 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from app import configurations
+    from app import configurations, routes
     from app.configurations import database, migration
 
     configurations.init_app(app)
     database.init_app(app)
     migration.init_app(app)
+    routes.init_app(app)
 
     @app.route("/")
     def hello_flask():
---
+++ app/users/route.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+users_bp = Blueprint("users", __name__, url_prefix="/api/users")
+
+
+@users_bp.route("/", methods=["GET"])
+def hello_users():
+    return {"msg": "Hello Users"}

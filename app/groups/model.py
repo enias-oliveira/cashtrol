b67f1ae4875fad77867f28e11b052645b019a54c
Enias Oliveira <eniasoliveira27@gmail.com>
2021-04-18 23:14:01 -0300

    feat: adds accounts and user to group relationship

--- app/groups/model.py
+++ app/groups/model.py
@@ -5,3 +5,17 @@ class GroupModel(db.Model):
     __tablename__ = "groups"
 
     id = db.Column(db.BigInteger, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    access_code = db.Column(db.String, nullable=False)
+    created_by = db.Column(
+        db.Integer,
+        db.ForeignKey(
+            ("users.id"),
+            onupdate="CASCADE",
+            ondelete="CASCADE",
+        ),
+    )
+
+    members_list = db.relationship(
+        "UserModel", backref="groups_list", secondary="accounts"
+    )

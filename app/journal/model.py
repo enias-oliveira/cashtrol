25409aa3e8131f213776ef45276d322f4946d5b0
Enias Oliveira <eniasoliveira27@gmail.com>
2021-04-20 23:24:24 -0300

    feat: adds creation method to journal and transactions

--- app/journal/model.py
+++ app/journal/model.py
@@ -25,6 +25,26 @@ class JournalModel(db.Model):
         ),
     )
 
+    created_at = db.Column(db.DateTime)
+
     transactions_list = db.relationship("TransactionModel", backref="entry")
 
     expense = db.relationship("ExpenseModel", uselist=False, backref="entry")
+
+    @classmethod
+    def create(cls, name: str, amount: float, group_id: int, created_by: id):
+        from flask import current_app
+
+        session = current_app.db.session
+
+        entry = cls(
+            name=name,
+            amount=amount,
+            group_id=group_id,
+            created_by=created_by,
+        )
+
+        session.add(entry)
+        session.commit()
+
+        return entry
--- app/transactions/model.py
+++ app/transactions/model.py
@@ -32,9 +32,39 @@ class TransactionModel(db.Model):
     )
 
     target_user = db.relationship(
-        "UserModel", backref="transactions_list", secondary="accounts"
+        "UserModel",
+        backref="transactions_list",
+        secondary="accounts",
+        viewonly=True,
     )
 
     group = db.relationship(
-        "GroupModel", backref="transactions_list", secondary="accounts"
+        "GroupModel",
+        backref="transactions_list",
+        secondary="accounts",
+        viewonly=True,
     )
+
+    @classmethod
+    def create(
+        cls,
+        amount: int,
+        type: TransactionType,
+        entry_id: int,
+        target_account: int,
+    ):
+        from flask import current_app
+
+        session = current_app.db.session
+
+        transaction = cls(
+            amount=amount,
+            type=type,
+            entry_id=entry_id,
+            target_account=target_account,
+        )
+
+        session.add(transaction)
+        session.commit()
+
+        return transaction

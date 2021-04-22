3395ad8b2e60a604176a7d6bd6ee3668e9b19a1b
Lucas <lucas.f.silva@provi.com.br>
2021-04-22 00:04:49 -0300

    faltando so validaçao de token

--- app/groups/services.py
+++ app/groups/services.py
@@ -1,5 +1,2 @@
 def get_invitation_code(data):
     return data['invite_code']
-    
-def verify_token(data):
-    ...
--- app/groups/view.py
+++ app/groups/view.py
@@ -1,20 +1,24 @@
 from flask import Blueprint, request
 from .services import get_invitation_code
 from .model import GroupModel
-from ..accounts import AccountModel
+from ..accounts.model import AccountModel
 from ..configurations.database import db
+from flask_jwt_extended import jwt_required, get_jwt_identity, JWTManager
 
-
-groups_bp = Blueprint("groups", __name__, url_prefix="/api/groups")
-
+groups_bp = Blueprint("groups", __name__, url_pre   fix="/api/groups")
 
 @groups_bp.route("/", methods=["GET"])
 def hello_groups():
     return {"msg": "Hello groups"}
 
 @groups_bp.route("/members", methods=["POST"])
+@jwt_required()
 def adding_new_member_to_group():
     data = request.get_json()
+    headers = request.headers['Authorization']
+    print('headers-----',headers.split()[1])
+    jwt = get_jwt_identity()
+    print('jwt---------', jwt)
     # Pegar token de autenticação e validar
         # --> como pegar o header pelo request?
     # Se existir aquele usuário, pegar o ID dentro do token de autenticação
@@ -29,8 +33,8 @@ def adding_new_member_to_group():
         # return {"error": "User already in group."}
     
     # se nao existir, criar nova conta conectando grupo ao usuário
-    new_account = AccountModel(group_id=group.id, user_id='blabla')
-    db.session.add(new_account)
+    # new_account = AccountModel(group_id=group.id, user_id='blabla')
+    # db.session.add(new_account)
     # retornar os dados do grupo atualizados
     accepted = {
         "id": group.id,

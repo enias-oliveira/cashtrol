39c39c1e1503ea89821fd62efe89f480e3337e33
Lucas <lucas.f.silva@provi.com.br>
2021-04-22 14:43:06 -0300

    just missing jwt authentication

--- app/groups/view.py
+++ app/groups/view.py
@@ -5,44 +5,42 @@ from ..accounts.model import AccountModel
 from ..configurations.database import db
 from flask_jwt_extended import jwt_required, get_jwt_identity, JWTManager
 
-groups_bp = Blueprint("groups", __name__, url_pre   fix="/api/groups")
+groups_bp = Blueprint("groups", __name__, url_prefix="/api/groups")
 
 @groups_bp.route("/", methods=["GET"])
 def hello_groups():
     return {"msg": "Hello groups"}
 
 @groups_bp.route("/members", methods=["POST"])
-@jwt_required()
+# @jwt_required()
 def adding_new_member_to_group():
+    # USAR o JWT pra pegar o ID
+    
     data = request.get_json()
     headers = request.headers['Authorization']
-    print('headers-----',headers.split()[1])
-    jwt = get_jwt_identity()
-    print('jwt---------', jwt)
-    # Pegar token de autenticação e validar
-        # --> como pegar o header pelo request?
-    # Se existir aquele usuário, pegar o ID dentro do token de autenticação
-        # --> se estivermos usando jwt, só decodificar e pegar a parte do ID
+    user_id = int(headers.split()[1])
+
+    # jwt = get_jwt_identity()
+    # print('jwt---------', jwt)
+
     code = get_invitation_code(data)
     table_group = db.session.query(GroupModel)
     group = table_group.filter(GroupModel.access_code == code).first()
     if not group:
         return {"error": "Group not found."}
 
-    # if group.is_member(user_id):
-        # return {"error": "User already in group."}
-    
-    # se nao existir, criar nova conta conectando grupo ao usuário
-    # new_account = AccountModel(group_id=group.id, user_id='blabla')
-    # db.session.add(new_account)
-    # retornar os dados do grupo atualizados
-    accepted = {
-        "id": group.id,
-        "name": group.name,
-        "invite_code": group.access_code,
-        "members": group.members_list
-    }
+    if group.is_member(user_id):
+        return {"error": "User already in group."}
+
+    # Se não for membro, adicionar a relação a table accounts
+    new_account = AccountModel(group_id=group.id, user_id=user_id)
+    db.session.add(new_account)
 
     db.session.commit()
 
-    return accepted, 204
+    return {
+        "id": group.id,
+        "name": group.name,
+        "invite_code": group.access_code,
+        "members": [user.id for user in group.members_list]
+    }
